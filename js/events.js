cc9261917c474ddc39265ce9e6a271ad5dbfd41c
Rui <ruisantos018@gmail.com>
2020-12-16 17:13:25 +0000

    added sort menu

--- js/events.js
+++ js/events.js
@@ -1,5 +1,7 @@
 const menuButton = document.querySelector('.menu-btn')
 const menu = document.querySelector('.menu')
+const sortButton = document.querySelector('#sortBtn')
+const sortMenu = document.querySelector('.sort')
 
 let menuOpen = false
 menuButton.addEventListener('click', () => {
@@ -14,3 +16,13 @@ menuButton.addEventListener('click', () => {
     }
 })
 
+let sortOpen = false
+sortButton.addEventListener('click', () => {
+   if(!sortOpen) {
+      sortMenu.classList.add('show')
+      sortOpen = true;
+   } else {
+      sortMenu.classList.remove('show')
+      sortOpen = false;
+   }
+})
